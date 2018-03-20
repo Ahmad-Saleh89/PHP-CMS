4937feec9d9a22706d538972e9bce619201b4490
unknown <ahmadsaleh1989@gmail.com>
2018-03-20 01:20:43 -0500

    blowfish crypt

--- mysql/functions.php
+++ mysql/functions.php
@@ -8,7 +8,16 @@ function createRow(){
     if(isset($_POST['submit'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
+        //  Using some sanitizing: 
+        $username = mysqli_real_escape_string($connection, $username);
+        $password = mysqli_real_escape_string($connection, $password);
 
+        // crypt password with Blowfish algorithm
+        $hashFormat = "$2y$10$";
+        $salt = "ichosecodingcozitshard";
+        $hashF_and_salt = $hashFormat . $salt;
+
+        $password = crypt($password, $hashF_and_salt);
         // Insert Values into users table in the database:
         $query = "INSERT INTO  users(username,password) ";
         $query .= "VALUES ('$username','$password')";
@@ -17,7 +26,7 @@ function createRow(){
         if(!$result) {
             die('Querry Failed' . mysqli_error());
         }else{
-            echo "<h4>Your Name and Password have been successfully added to our database!!</h4>";
+            echo "<h4>Your Name and Password are successfully added to our database!!</h4>";
         }
     }
 }
@@ -32,6 +41,7 @@ function showId(){
     }
 
     while($row = mysqli_fetch_assoc($result)){
+        /* Here we are only fetching the value of id from users tabel */
         $id = $row['id'];
         echo "<option value='$id'>$id</option>";
     }
