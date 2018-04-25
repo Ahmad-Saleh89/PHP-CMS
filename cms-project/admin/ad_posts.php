3f8d5e14fc1806745e3c9abf00f9d24afce6bfb0
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-04-25 10:29:42 -0500

    Editing Admin post area

---
+++ cms-project/admin/ad_posts.php
@@ -0,0 +1,70 @@
+<?php include "includes/ad_header.php"; ?>
+
+<body>
+
+	<div id="wrapper">
+
+		<!-- Navigation -->
+		<?php include "includes/ad_navigation.php" ?>
+
+		<div id="page-wrapper">
+
+			<div class="container-fluid">
+
+				<!-- Page Heading -->
+				<div class="row">
+					<div class="col-lg-12">
+						<h1 class="page-header">
+								Posts
+						</h1>
+					</div>
+					
+					<table class="table table-bordered table-hover">
+						<thead>
+							<tr>
+								<th>Id</th>
+								<th>Author</th>
+								<th>Title</th>
+								<th>Category</th>
+								<th>Status</th>
+								<th>Image</th>
+								<th>Tags</th>
+								<th>Comments</th>
+								<th>Date</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+
+				</div> <!-- /.row -->
+				
+			</div>
+			<!-- /.container-fluid -->
+
+		</div>
+		<!-- /#page-wrapper -->
+
+	</div>
+	<!-- /#wrapper -->
+
+	<!-- jQuery -->
+	<script src="js/jquery.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
